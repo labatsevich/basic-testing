e798391ca08875e6bdc939df746b577c3c69fd60
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-28 15:47:52 +0300

    feat: 03-error-handking completed

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,39 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  resolveValue,
+  throwCustomError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    expect(resolveValue(123)).resolves.toBe(123);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    const message = 'Something went wrong!';
+    expect(() => throwError(message)).toThrow(message);
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    const messageDefault = 'Oops!';
+    expect(throwError).toThrow(messageDefault);
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    const message = 'This is my awesome custom error!';
+    expect(throwCustomError).toThrow(message);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    const message = 'This is my awesome custom error!';
+    expect(rejectCustomError).rejects.toThrow(message);
   });
 });
