9af66830dd62b78d4b7e79df4aee778845a11c5d
yurilabatsevich <yurilabatsevich@gmail.com>
2023-06-30 00:28:57 +0300

    feat: covered four test 06-mocking-node-api

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,5 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +11,21 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const cb = jest.fn();
+    const spyTimeout = jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(cb, 100);
+    expect(spyTimeout).toHaveBeenCalledTimes(1);
+    expect(spyTimeout).toHaveBeenLastCalledWith(cb, 100);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const cb = jest.fn();
+    const spyTimeout = jest.spyOn(global, 'setTimeout');
+    doStuffByTimeout(cb, 100);
+    expect(cb).not.toBeCalled();
+    jest.runOnlyPendingTimers();
+    expect(spyTimeout).toBeCalled();
+    expect(spyTimeout).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -29,11 +39,20 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const cb = jest.fn();
+    const spyInterval = jest.spyOn(global, 'setInterval');
+    doStuffByInterval(cb, 1000);
+    expect(spyInterval).toBeCalled();
+    expect(spyInterval).toHaveBeenLastCalledWith(cb, 1000);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const cb = jest.fn();
+    doStuffByInterval(cb, 1000);
+    expect(cb).not.toBeCalled();
+    jest.advanceTimersByTime(10000);
+    expect(cb).toBeCalled();
+    expect(cb).toBeCalledTimes(10);
   });
 });
 
